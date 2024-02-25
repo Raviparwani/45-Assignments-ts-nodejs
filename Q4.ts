e6febfb94f341b924b6970970510fc3208c5cdc7
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-26 01:00:23 +0500

    Delete Q4.ts

--- Q4.ts
+++
@@ -1,6 +0,0 @@
-// 4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the
-// following, including the quotation marks:
-
-let name1: string= (" Walt Disney said")
-
-console.log(`${name1}, “The way to get started is to quit talking and begin doing.” `)
