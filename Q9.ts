298c8646448d980376fb5f7e14819417f7664dd0
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-26 01:03:14 +0500

    Delete Q9.ts

--- Q9.ts
+++
@@ -1,7 +0,0 @@
-// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, 
-//create a message that reveals your favorite number. Print that message.
-
-
-
-let num: number = 27
-console.log(`My Favourite Number Is ${num}`)
