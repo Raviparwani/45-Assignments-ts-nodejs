8c26b329422bf5a88108b68248f263d3f4efb9ac
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-26 01:00:51 +0500

    Delete Q5.ts

--- Q5.ts
+++
@@ -1,9 +0,0 @@
-// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable
-// called famous_person. Then compose your message and store it in a new variable called message.
-// Print your message.
-
-
-let famous_person:String = "Eleanor Roosevelt"
-let message: string = "The future belongs to those who believe in the beauty of their dreams."
-
-console.log(`${famous_person} said. “${message}”`)
