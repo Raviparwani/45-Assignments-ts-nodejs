9798861e762527dddae345dd6f43ac2ee68b49e4
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-26 01:00:37 +0500

    Delete Q5.js

--- Q5.js
+++
@@ -1,4 +0,0 @@
-// 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-var famous_person = "Eleanor Roosevelt";
-var message = "The future belongs to those who believe in the beauty of their dreams.";
-console.log("".concat(famous_person, " said. \u201C").concat(message, "\u201D"));
