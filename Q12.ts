5f33f434172c908b86928570006b577303760356
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-26 01:04:34 +0500

    Delete Q12.ts

--- Q12.ts
+++
@@ -1,9 +0,0 @@
-//11. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
-// print a message to them. The text of each message should be the same, but each message should be 
-// personalized with the person’s name.
-
-let names: string[] = ["Shahbaz Ahmed", "Ghouse Ahmed", "Waqas Ahmed"];
-let message: string = "Do you like to play Cicket ";
-console.log(names[0] + " " + message);
-console.log(names[1] + " " + message);
-console.log(names[2] + " " + message);
