e84aebd37cd6df04fa3a536d317c1cafc4f6c92e
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-26 01:04:00 +0500

    Delete Q11.js

--- Q11.js
+++
@@ -1,7 +0,0 @@
-//10. Names: Store the names of a few of your friends in a array called names. 
-// Print each person’s name by accessing each element in the list, one at a time.
-var names = ["Najeeb Ahmed", "Waqar Ahmed", "Shahbaz Ahmed", "Ghouse Ahmed"];
-console.log(names[0]);
-console.log(names[1]);
-console.log(names[2]);
-console.log(names[3]);
