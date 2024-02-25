ec9c3a3b3a5ee5d4ed9d05e5820eca5b49789d4e
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-26 01:04:22 +0500

    Delete Q12.js

--- Q12.js
+++
@@ -1,8 +0,0 @@
-//11. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, 
-// print a message to them. The text of each message should be the same, but each message should be 
-// personalized with the person’s name.
-var names = ["Shahbaz Ahmed", "Ghouse Ahmed", "Waqas Ahmed"];
-var message = "Do you like to play Cicket ";
-console.log(names[0] + " " + message);
-console.log(names[1] + " " + message);
-console.log(names[2] + " " + message);
