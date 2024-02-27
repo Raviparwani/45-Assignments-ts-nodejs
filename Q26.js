1c260e8e10693a84649b71fe9508f263ac5d04c2
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-27 13:19:13 +0500

    Update Q26.js

--- Q26.js
+++ Q26.js
@@ -1,21 +1,26 @@
+"use strict";
 // 26. Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and
 // write an if-else chain.
 // • If the alien’s color is green, print a statement that the
 // player just earned 5 points for shooting the alien.
 // • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
 // • Write one version of this program that runs the if block and another that runs the else block.
-var alien_color = 'green';
-if (alien_color === 'green') {
-    console.log("You just earned 5 points for shooting the green alien.");
+// Version that runs the if block (alien_color is 'green')
+let alien_color_if = 'green';
+if (alien_color_if === 'green') {
+    console.log("• Version that runs the if block alien_color is 'green'\n");
+    console.log("Player just earned 5 points for shooting the alien!");
 }
 else {
-    console.log("You just earned 10 points for shooting an alien of a different color.");
+    console.log("Player just earned 10 points.");
 }
-// Version that passes the else condition
-// let alien_color: string = 'green';
-if (alien_color === 'red') {
-    console.log("You just earned 5 points for shooting the green alien.");
+console.log("-".repeat(50));
+// Version that runs the else block (alien_color is 'red')
+let alien_color_else = 'red';
+if (alien_color_else === 'green') {
+    console.log("Player just earned 5 points for shooting the alien!");
 }
 else {
-    console.log("You just earned 10 points for shooting an alien of a different color.");
+    console.log("• Version that runs the else block alien_color is 'red'\n");
+    console.log("Player just earned 10 points.");
 }
