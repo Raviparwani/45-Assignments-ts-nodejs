58a35cef2811ef59df55c4064f56fef82b87d8a3
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-27 10:33:07 +0500

    Update Q21.ts

--- Q21.ts
+++ Q21.ts
@@ -1,7 +1,35 @@
 // Q21. They think of something you could store in a TypeScript Object. Write a program that
 // creates Objects containing these items.
 
+// example1
+console.log("\nExample1","-".repeat(50))
+
+
 let Product:{
     Name:string, Price:number, Qty:number ,Sale:number, Bal:number} = {
-        Name:"Brufen Syrup", Price:100, Qty:500 ,Sale:50, Bal:450}
+        Name:"Lysovit Syrup", Price:100, Qty:500 ,Sale:50, Bal:450}
 console.log(Product)
+
+
+//example 2
+console.log("\nExample2","-".repeat(50))
+
+const Brufen = {
+    Name : "Brufen Syrup",
+    Price : 100,
+    Qty : 500,
+    Sale : 50,
+    Bal : 450
+}
+const Panadol = {
+    Name : "Panadol Syrup",
+    Price : 100,
+    Qty : 300,
+    Sale : 100,
+    Bal : 400,
+}
+
+console.log(Brufen,"\n",Panadol)
+
+
+
