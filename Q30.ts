4363cdaba4b677d1bf229cf1fb3790dd390ef8ba
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-27 13:22:24 +0500

    Update Q30.ts

--- Q30.ts
+++ Q30.ts
@@ -4,16 +4,28 @@
 //• If the username is 'admin', print a special greeting, such as Hello admin,
 // would you like to see a status report?
 
-//• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 
-let users: string[] =["Admin", "Fiza", "Ahmed", "Faizan", "Muneeb"];
-for(let i=0; i<users.length; i++){
-    if(users[i] == "Admin"){
+//Example in for loop
+console.log("• Example in for loop")
+let usernames: string[] =["Admin", "Fiza", "Ahmed", "Faizan", "Muneeb"];
+for(let i=0; i<usernames.length; i++){
+    if(usernames[i] == "Admin"){
         console.log("Hello Admin, would you like to see a status report?")
     }
     else{
-        console.log(`Hello ${users[i]} Thank you for logging in again.`)
+        console.log(`Hello ${usernames[i]} Thank you for logging in again.`)
     }
 }
 
+console.log("-".repeat(50))
 
+//Example in for of loop
+console.log("• Example in for of loop")
+for (let username of usernames) {
+  if (username === 'admin') {
+    console.log("Hello admin, would you like to see a status report?");
+  } else {
+    console.log(`Hello ${username}, thank you for logging in again.`);
+  }
+}
