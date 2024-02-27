595eb475cb73800b56d44aa06c6707aa858a1911
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-27 10:32:17 +0500

    Update Q21.js

--- Q21.js
+++ Q21.js
@@ -1,6 +1,26 @@
+"use strict";
 // Q21. They think of something you could store in a TypeScript Object. Write a program that
 // creates Objects containing these items.
-var Product = {
-    Name: "Brufen Syrup", Price: 100, Qty: 500, Sale: 50, Bal: 450
+// example1
+console.log("\nExample1", "-".repeat(50));
+let Product = {
+    Name: "Lysovit Syrup", Price: 100, Qty: 500, Sale: 50, Bal: 450
 };
 console.log(Product);
+//example 2
+console.log("\nExample2", "-".repeat(50));
+const Brufen = {
+    Name: "Brufen Syrup",
+    Price: 100,
+    Qty: 500,
+    Sale: 50,
+    Bal: 450
+};
+const Panadol = {
+    Name: "Panadol Syrup",
+    Price: 100,
+    Qty: 300,
+    Sale: 100,
+    Bal: 400,
+};
+console.log(Brufen, "\n", Panadol);
