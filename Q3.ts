56fddd8fd1b851084743e0c744a61f03c5902742
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-03-01 16:28:19 +0500

    Update Q3.ts

--- Q3.ts
+++ Q3.ts
@@ -2,25 +2,26 @@
 //  lowercase, uppercase, and titlecase.
 
 //Example 1
-console.log("\nExample1","-".repeat(50))
+console.log("\nExample 1","-".repeat(50))
 let firstName: string = "ravi kumar";
 
-console.log(firstName.toLocaleLowerCase());
-console.log(firstName.toLocaleUpperCase());
-
+console.log(`${firstName.toLocaleLowerCase()}\t"lowercase"`);
+console.log(`${firstName.toLocaleUpperCase()}\t"UPPERCASE"`);
+console.log(`${firstName.replace(/\b\w/g,(char) => char.toUpperCase())}\t"Title Case"`)
 //example 2 for lowercase
 console.log("\nExample2","-".repeat(50))
 
 let name0: string = "Shahbaz ahmed"
 let name2:string = name0.toLowerCase();
-console.log(name2)
+console.log(`${name2}\t"lowercase"`)
 
 //example 2 for uppercase
 let name3: string = "mukesh kumar"
 let name4:string = name3.toUpperCase();
-console.log(name4)
+console.log(`${name4}\t"UPPERCASE"`)
 
-//example  for titlecase
+
+// //example  for titlecase
 let nameToConvert: string = "ravi kumar parwani";
 
 // Split the name into an array of words
@@ -29,4 +30,6 @@ let words: string[] = nameToConvert.split(' ');
 // Capitalize the first letter of each word
 let titleCaseName: string = words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 
-console.log(titleCaseName);
+console.log(`${titleCaseName}\t"Title Case"`);
+
+
